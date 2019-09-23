d7be6c1dc300ff96f34715bef0f63e2250a2c3eb
Louis6785 <louis.shi@jabil.com>
2019-09-24 00:52:03 +0800

    設置webpack開發與生產模式

---
+++ webpack.dev.js
@@ -0,0 +1,10 @@
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+    mode: 'development',
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist'//啟動服務時檔案讀取的路徑
+    }
+});
