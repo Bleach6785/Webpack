eba5107e97421f461279341fb39d6d7a114b4c68
Louis6785 <louis.shi@jabil.com>
2019-09-23 20:59:19 +0800

    安裝自動產生html檔案插件(html-webpack-plugin)

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 var path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin'); //引入清除文件插件
+const HtmlWebpackPlugin = require('html-webpack-plugin'); //引入自動產生html檔案插件
 
 module.exports = {
   mode: 'development',
@@ -11,5 +12,9 @@ module.exports = {
   // 由于plugin可以携带参数/选项，必须在wepback配置中，向plugins属性传入new实例
   plugins: [
     new CleanWebpackPlugin(),//实例化，参数为目录
+    new HtmlWebpackPlugin({
+        template: "./src/index.html",
+        inject: true
+    }),
   ],
 };
