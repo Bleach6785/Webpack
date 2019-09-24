4d6b6b12f1b319c939f997361dbb819bc5d4b8b6
Louis6785 <louis.shi@jabil.com>
2019-09-24 11:10:32 +0800

    使用 postcss-loader 與 autoprefixer 套件自動增加css瀏覽器前綴

--- webpack.common.js
+++ webpack.common.js
@@ -47,6 +47,16 @@ module.exports = {
           
           // "style-loader", // 将 JS 字符串生成为 style 节点
           "css-loader", // 将 CSS 转化成 CommonJS 模块
+          {
+            loader: 'postcss-loader', // Run postcss actions
+            options: {
+              plugins: function () { // postcss plugins, can be exported to postcss.config.js
+                return [
+                  require('autoprefixer')
+                ];
+              }
+            }
+          },
           "sass-loader" // 将 Sass 编译成 CSS，默认使用 Node Sass
         ]
       },
