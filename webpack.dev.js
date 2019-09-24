9d0d638098a99e3aed458ed8a861216493269b4b
Louis6785 <louis.shi@jabil.com>
2019-09-24 10:07:11 +0800

    在開發環境中添加 devServer.writeToDisk = true的屬性，以便在 dist 中產生相關的檔案(html、css、js)

--- webpack.dev.js
+++ webpack.dev.js
@@ -5,6 +5,7 @@ module.exports = merge(common, {
     mode: 'development',
     devtool: 'inline-source-map',
     devServer: {
-        contentBase: './dist'//啟動服務時檔案讀取的路徑
+        contentBase: './dist',//啟動服務時檔案讀取的路徑
+        writeToDisk: true,
     }
 });
